c33814afa32e314006fbd8fc45db866280d4e01c
izabela <izabel.kulak@gmail.com>
2017-08-15 18:52:01 +0200

    Time picker

--- app/src/main/java/com/example/izabela/analogdigitalclock/MainActivity.java
+++ app/src/main/java/com/example/izabela/analogdigitalclock/MainActivity.java
@@ -6,8 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import static com.example.izabela.analogdigitalclock.R.id.countdown;
 
@@ -20,8 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText countdownNumber = (EditText) findViewById(R.id.countSecondsNumber);
+        //final EditText countdownNumber = (EditText) findViewById(R.id.countSecondsNumber);
         final TextView countdownView = (TextView) findViewById(countdown);
+        final TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        timePicker.setIs24HourView(true);
+        timePicker.setCurrentMinute(0);
+        timePicker.setCurrentHour(0);
+
 
 
         final Button startCountingButton = (Button) findViewById(R.id.countdownButton);
@@ -31,16 +36,14 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                String number = countdownNumber.getText().toString();
-                long timeToCountdown = 0;
-                if(!number.isEmpty() && number != null) {
-                     timeToCountdown = Long.parseLong(number);
-                }
-                if(timeToCountdown != 0) {
-                    countdownNumber.setEnabled(false);
+                System.out.println("" + timePicker.getCurrentHour() + ":" + timePicker.getCurrentMinute());
+                long secFromTimePicker = timePicker.getCurrentHour()*60*60+timePicker.getCurrentMinute()*60;
+
+                if(secFromTimePicker != 0) {
+                    timePicker.setEnabled(false);
                     startCountingButton.setEnabled(false);
                     stopCountingButton.setEnabled(true);
-                    countDownTimer = getCountDownTimer(timeToCountdown*1000);
+                    countDownTimer = getCountDownTimer(secFromTimePicker*1000);
                     countDownTimer.start();
                 }
             }
@@ -50,16 +53,27 @@ public class MainActivity extends AppCompatActivity {
                 return new CountDownTimer(timeToCountdown, 1000) {
                     @Override
                     public void onTick(long l) {
-                        countdownView.setText(String.valueOf(l / 1000));
+                        long allSec = l / 1000;
+                        long min = allSec/60;
+                        long hours = min/60;
+                        if(min>=60) {
+                            min = min % 60;
+                        }
+                        if(hours>=24) {
+                            hours = hours % 60;
+                        }
+                        long sec = allSec % 60;
+                        countdownView.setText(hours + " : " + min + " : " + sec);
 
                     }
 
                     @Override
                     public void onFinish() {
                         countdownView.setText(R.string.done);
-                        countdownNumber.setEnabled(true);
+                        timePicker.setEnabled(true);
                         startCountingButton.setEnabled(true);
-                        countdownNumber.setText("");
+                        timePicker.setCurrentHour(0);
+                        timePicker.setCurrentMinute(0);
                     }
                 };
             }
@@ -72,8 +86,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 stopCountingButton.setEnabled(false);
                 startCountingButton.setEnabled(true);
-                countdownNumber.setEnabled(true);
-                countdownNumber.setText("");
+                timePicker.setEnabled(true);
+                timePicker.setCurrentHour(0);
+                timePicker.setCurrentMinute(0);
                 countdownView.setText("");
                 countDownTimer.cancel();
 
